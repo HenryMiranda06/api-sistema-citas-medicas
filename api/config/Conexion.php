75bf677effeca15a92ee0935039bdfebbba3c8d3
Henry Miranda <mhenry2406@gmail.com>
2026-06-30 10:55:01 -0600

    Conexion a la bd

--- api/config/Conexion.php
+++ api/config/Conexion.php
@@ -1,25 +1,21 @@
 <?php
-
-
 class Conexion
 {
-
-    private $host = "localhost";
-    private $dbname = "bd_sistema_citas";
+    private $host = "zephyr.proxy.rlwy.net";
+    private $port = "13809";
+    private $dbname = "db_sistema_citas";
     private $user = "root";
-    private $password = "";
+    private $password = "WbRNpaUVwBWsTdOsCLqIOunVFpRtPaHV";
 
     public function Conectar()
     {
         try {
             $conexion = new PDO(
-                "mysql:host={$this->host};dbname={$this->dbname}",
+                "mysql:host={$this->host};port={$this->port};dbname={$this->dbname}",
                 $this->user,
                 $this->password
             );
-
             $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
             return $conexion;
         } catch (PDOException $th) {
             die($th->getMessage());
